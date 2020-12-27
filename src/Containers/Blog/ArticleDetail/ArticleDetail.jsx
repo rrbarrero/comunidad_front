19cdfd2ca60f4d435775f4b7573e887fb97e3eb6
Roberto <rbarrero@posteo.net>
2020-12-27 22:45:48 +0100

    Continuamos

--- src/Containers/Blog/ArticleDetail/ArticleDetail.jsx
+++ src/Containers/Blog/ArticleDetail/ArticleDetail.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import FetchArticleDetail from '../../../Services/Blog/FetchArticleDetail';
+import Spinner from '../../../Assets/spinner.gif';
 
-const ArticleDetail = ({articuloId}) => {
+const ArticleDetail = ({ articuloId }) => {
+    
+    const [article, setArticle] = useState({});
+    const [isLoading, setIsLoading] = useState();
+
+    useEffect(() => {
+        if (articuloId) {
+            setIsLoading(true);
+            let isSubscribed = true;
+            FetchArticleDetail(articuloId).then(resp => {
+                if (isSubscribed) {
+                    setArticle(resp);
+                    setIsLoading(false);
+                }
+            });
+            return () => isSubscribed = false;
+        }
+    }, [articuloId]);
+
 
     return (
-        <h2>{articuloId}</h2>
+        <>
+        <p className="text-2xl font-bold">{article.titulo}</p>
+        <div className="w-full p-5">
+                {!isLoading && <img className="w-52 lg:w-52 float-left m-5 rounded" src={article.imagen} alt="Artículo imagen" />}
+                {isLoading && <img className="float-left m-5 rounded" src={Spinner} alt="Artículo imagen" />}
+                <article className="text-sm " dangerouslySetInnerHTML={{ __html: article.cuerpo }}></article>
+        </div>
+        </>
     );
 }
 
---
+++ src/Services/Blog/FetchArticleDetail.js
@@ -0,0 +1,18 @@
+const FetchCommentsOfArticle = (articleId) => {    
+    /*
+        RETURN articleId DETAILS
+    */
+    let url = `${process.env.REACT_APP_API_URL}blog/articulos/${articleId}`;
+    
+    const response = fetch(url, {
+        method: 'get',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }).then(resp => resp.json())
+        .catch(error => console.log(error));
+    return response;
+}
+
+export default FetchCommentsOfArticle;
