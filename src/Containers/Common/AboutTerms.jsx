8c4aa247acc533662caba5d6080be62274f99dcf
Roberto Rodríguez <roberto.rodriguez@juntaex.es>
2021-01-21 10:33:07 +0100

    Arreglo en el texto legal

--- src/Containers/Common/AboutTerms.jsx
+++ src/Containers/Common/AboutTerms.jsx
@@ -3,7 +3,6 @@ import Navbar from '../Navbar/Navbar';
 import NavbarMobile from '../Navbar/NavbarMobile';
 import Sidebar from '../Sidebar/Siderbar';
 import Footer from '../Footer/Footer';
-import './AboutTerms.css';
 
 const AboutTerms = () => {
     return (
