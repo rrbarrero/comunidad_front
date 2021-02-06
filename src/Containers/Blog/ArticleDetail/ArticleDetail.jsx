f5321361236606bda1431c0b0b7d710a3f61c5d2
Roberto Rodríguez <rbarrero@posteo.net>
2021-02-06 09:55:43 +0100

    Ponemos botones de compartir tambien en los hilos

--- src/Containers/Blog/ArticleDetail/ArticleDetail.jsx
+++ src/Containers/Blog/ArticleDetail/ArticleDetail.jsx
@@ -15,226 +15,226 @@ import FetchUserDetail from '../../../Services/User/FetchUserDetail';
 import { getDateFormated, getSignature } from '../../../Services/Common/Misc';
 import { FaComments } from 'react-icons/fa';
 import { TwitterShareButton, FacebookShareButton, WhatsappShareButton } from 'react-share';
-import {TwitterIcon, FacebookIcon, WhatsappIcon} from 'react-share';
+import { TwitterIcon, FacebookIcon, WhatsappIcon } from 'react-share';
 
 
 const ArticleDetail = () => {
-    
-    const { pathname, hash } = useLocation();
-    const { articuloId } = useParams();
 
-    const [article, setArticle] = useState({});
-    const [isLoading, setIsLoading] = useState();
-    const [comentarios, setComentarios] = useState([]);
-    const [updatingComment, setUpdatingComment] = useState(false);
-    const [commentToUpdate, setCommentToUpdate] = useState({});
-    const [autor, setAutor] = useState('');
+  const { pathname, hash } = useLocation();
+  const { articuloId } = useParams();
 
-    const hashtags = ["ComunidadLSA", "CongresoLSA_25Enero"];
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState();
+  const [comentarios, setComentarios] = useState([]);
+  const [updatingComment, setUpdatingComment] = useState(false);
+  const [commentToUpdate, setCommentToUpdate] = useState({});
+  const [autor, setAutor] = useState('');
+
+  const hashtags = ["ComunidadLSA", "AprenderEsUnaActitud"];
 
 
-    useEffect(() => {
-        if (articuloId) {
-            setIsLoading(true);
-            let isSubscribed = true;
-            FetchArticleDetail(articuloId).then(resp => {
-                if (isSubscribed) {
-                    setArticle(resp);
-                }
-            });
-            FetchCommentsOfArticle(articuloId).then(resp => {
-                if (isSubscribed) {
-                    setComentarios(resp.results);
-                    setIsLoading(false);
-                }
-            });
-            return () => isSubscribed = false;
+  useEffect(() => {
+    if (articuloId) {
+      setIsLoading(true);
+      let isSubscribed = true;
+      FetchArticleDetail(articuloId).then(resp => {
+        if (isSubscribed) {
+          setArticle(resp);
         }
-    }, [articuloId]);
-
-    useEffect(() => {
-        if (article && article.autor) {
-            let isSubscribed = true;
-            FetchUserDetail(article.autor).then(resp => {
-                if (isSubscribed) {
-                    setAutor(resp);
-                }
-            });
-            return () => isSubscribed = false;
+      });
+      FetchCommentsOfArticle(articuloId).then(resp => {
+        if (isSubscribed) {
+          setComentarios(resp.results);
+          setIsLoading(false);
         }
-    },[article]);
+      });
+      return () => isSubscribed = false;
+    }
+  }, [articuloId]);
 
-    useEffect(() => {
-      // if not a hash link scroll to top
-      if(hash===''){
-          window.scrollTo(0, 0)
-      }
-      // else scroll to id
-      else{
-          setTimeout(
-              () => {
-                  const id = hash.replace('#', '');
-                  const element = document.getElementById(id);
-                  if (element) {
-                      element.scrollIntoView({behavior: "smooth"});
-                  }
-              },
-              1000
-          );
-      }
-    }, [hash, pathname])
+  useEffect(() => {
+    if (article && article.autor) {
+      let isSubscribed = true;
+      FetchUserDetail(article.autor).then(resp => {
+        if (isSubscribed) {
+          setAutor(resp);
+        }
+      });
+      return () => isSubscribed = false;
+    }
+  }, [article]);
+
+  useEffect(() => {
+    // if not a hash link scroll to top
+    if (hash === '') {
+      window.scrollTo(0, 0)
+    }
+    // else scroll to id
+    else {
+      setTimeout(
+        () => {
+          const id = hash.replace('#', '');
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        },
+        1000
+      );
+    }
+  }, [hash, pathname])
 
 
-    return (
-      <div className="w-full overflow-hidden sm:w-3/4">
-        <div className="px-1 pt-6">
-          <p className="text-2xl font-bold pl-2">{article.titulo}</p>
-          <div className="w-full p-5">
-            {!isLoading && (
-              <img
-                className="w-52 lg:w-52 float-left m-1 rounded"
-                src={article.imagen}
-                alt="Artículo imagen"
-              />
-            )}
-            {isLoading && (
-              <img
-                className="float-left m-5 rounded"
-                src={Spinner}
-                alt="Artículo imagen"
-              />
-            )}
-            <article
-              id="article-detail"
-              dangerouslySetInnerHTML={{ __html: article.cuerpo }}
-            ></article>
-          </div>
+  return (
+    <div className="w-full overflow-hidden sm:w-3/4">
+      <div className="px-1 pt-6">
+        <p className="text-2xl font-bold pl-2">{article.titulo}</p>
+        <div className="w-full p-5">
+          {!isLoading && (
+            <img
+              className="w-52 lg:w-52 float-left m-1 rounded"
+              src={article.imagen}
+              alt="Artículo imagen"
+            />
+          )}
+          {isLoading && (
+            <img
+              className="float-left m-5 rounded"
+              src={Spinner}
+              alt="Artículo imagen"
+            />
+          )}
+          <article
+            id="article-detail"
+            dangerouslySetInnerHTML={{ __html: article.cuerpo }}
+          ></article>
+        </div>
 
-          <div className="flex justify-between md:w-4/5 items-center h-16 p-2 mt-2 mb-8 border-2 bg-white border-blue-congreso100 shadow-xl rounded-lg">
-            <div className="flex w-full items-center">
-              <div className="flex w-11/12 sm:w-9/12">
-                <Avatar userId={article.autor} />
-                <br />
-                <div className="pl-1">
-                  <div className="text-sm text-red-congreso200 mt-3">
-                    Por{" "}
-                    <span className="text-sm font-semibold">
-                      {autor && getSignature(autor)}
-                    </span>{" "}
-                    <span className="text-sm text-red-congreso100">
-                      {getDateFormated(article.fecha_creacion)}
-                    </span>
-                  </div>
-                  <div className="hidden lg:flex lg:items-center lg:w-auto text-sm font-light italic text-gray-congreso200">
-                    {autor.perfil && autor.perfil.frase_inspiradora}
-                  </div>
+        <div className="flex justify-between md:w-4/5 items-center h-16 p-2 mt-2 mb-8 border-2 bg-white border-blue-congreso100 shadow-xl rounded-lg">
+          <div className="flex w-full items-center">
+            <div className="flex w-11/12 sm:w-9/12">
+              <Avatar userId={article.autor} />
+              <br />
+              <div className="pl-1">
+                <div className="text-sm text-red-congreso200 mt-3">
+                  Por{" "}
+                  <span className="text-sm font-semibold">
+                    {autor && getSignature(autor)}
+                  </span>{" "}
+                  <span className="text-sm text-red-congreso100">
+                    {getDateFormated(article.fecha_creacion)}
+                  </span>
                 </div>
-              </div>
-              <div className="flex w-1/12 flex-shrink-0">
-                <div className="text-red-congreso100 text-lg">
-                  {comentarios.length}{" "}
-                  <FaComments className="ml-1 border-transparent" />
+                <div className="hidden lg:flex lg:items-center lg:w-auto text-sm font-light italic text-gray-congreso200">
+                  {autor.perfil && autor.perfil.frase_inspiradora}
                 </div>
-              </div>
-              <div className="hidden sm:flex w-2/12 sm:flex-shrink-0">
-                <TwitterShareButton
-                  url={process.env.REACT_APP_FRONTEND_URL + pathname}
-                  title={article.titulo}
-                  hashtags={hashtags}
-                  related={["AprendizajeExt"]}
-                >
-                  <TwitterIcon size={32} round={true} />
-                </TwitterShareButton>
-                <FacebookShareButton
-                  url={process.env.REACT_APP_FRONTEND_URL + pathname}
-                  quote={article.titulo + " " + article.entradilla}
-                  hashtags={hashtags}
-                >
-                  <FacebookIcon size={32} round={true} />
-                </FacebookShareButton>
-                <WhatsappShareButton
-                  url={process.env.REACT_APP_FRONTEND_URL + pathname}
-                  title={article.titulo}
-                >
-                  <WhatsappIcon size={32} round={true} />
-                </WhatsappShareButton>
               </div>
             </div>
-          </div>
-          <div className="sm:hidden flex w-full justify-center">
-            <div className="mx-1">
+            <div className="flex w-1/12 flex-shrink-0">
+              <div className="text-red-congreso100 text-lg">
+                {comentarios.length}{" "}
+                <FaComments className="ml-1 border-transparent" />
+              </div>
+            </div>
+            <div className="hidden sm:flex w-2/12 sm:flex-shrink-0">
               <TwitterShareButton
                 url={process.env.REACT_APP_FRONTEND_URL + pathname}
                 title={article.titulo}
                 hashtags={hashtags}
                 related={["AprendizajeExt"]}
               >
-                <TwitterIcon size={42} round={true} />
+                <TwitterIcon size={32} round={true} />
               </TwitterShareButton>
-            </div>
-            <div className="mx-1">
               <FacebookShareButton
                 url={process.env.REACT_APP_FRONTEND_URL + pathname}
                 quote={article.titulo + " " + article.entradilla}
                 hashtags={hashtags}
               >
-                <FacebookIcon size={42} round={true} />
+                <FacebookIcon size={32} round={true} />
               </FacebookShareButton>
-            </div>
-            <div className="mx-1">
               <WhatsappShareButton
                 url={process.env.REACT_APP_FRONTEND_URL + pathname}
                 title={article.titulo}
               >
-                <WhatsappIcon size={42} round={true} />
+                <WhatsappIcon size={32} round={true} />
               </WhatsappShareButton>
             </div>
           </div>
-
-          <div id="article-detail-comments" className="">
-            <p className="text-xl font-bold p-3">Comentarios</p>
-            <div>
-              {comentarios.map((comment, i) => {
-                if (
-                  updatingComment &&
-                  commentToUpdate &&
-                  commentToUpdate.id === comment.id
-                ) {
-                  return (
-                    <UpdateComment
-                      key={i}
-                      comment={comment}
-                      comentarios={comentarios}
-                      setComentarios={setComentarios}
-                      setUpdatingComment={setUpdatingComment}
-                      updateFunction={UpdateCommentArticle}
-                    />
-                  );
-                } else {
-                  return (
-                    <CommentArticle
-                      key={i}
-                      comment={comment}
-                      commentIdx={i}
-                      setUpdatingComment={setUpdatingComment}
-                      setCommentToUpdate={setCommentToUpdate}
-                    />
-                  );
-                }
-              })}
-            </div>
-          </div>
-          {updatingComment === false && (
-            <PostComment
-              article={article}
-              comentarios={comentarios}
-              setComentarios={setComentarios}
-              postCommentFunction={PostCommentArticle}
-            />
-          )}
         </div>
+        <div className="sm:hidden flex w-full justify-center">
+          <div className="mx-1">
+            <TwitterShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              title={article.titulo}
+              hashtags={hashtags}
+              related={["AprendizajeExt"]}
+            >
+              <TwitterIcon size={42} round={true} />
+            </TwitterShareButton>
+          </div>
+          <div className="mx-1">
+            <FacebookShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              quote={article.titulo + " " + article.entradilla}
+              hashtags={hashtags}
+            >
+              <FacebookIcon size={42} round={true} />
+            </FacebookShareButton>
+          </div>
+          <div className="mx-1">
+            <WhatsappShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              title={article.titulo}
+            >
+              <WhatsappIcon size={42} round={true} />
+            </WhatsappShareButton>
+          </div>
+        </div>
+
+        <div id="article-detail-comments" className="">
+          <p className="text-xl font-bold p-3">Comentarios</p>
+          <div>
+            {comentarios.map((comment, i) => {
+              if (
+                updatingComment &&
+                commentToUpdate &&
+                commentToUpdate.id === comment.id
+              ) {
+                return (
+                  <UpdateComment
+                    key={i}
+                    comment={comment}
+                    comentarios={comentarios}
+                    setComentarios={setComentarios}
+                    setUpdatingComment={setUpdatingComment}
+                    updateFunction={UpdateCommentArticle}
+                  />
+                );
+              } else {
+                return (
+                  <CommentArticle
+                    key={i}
+                    comment={comment}
+                    commentIdx={i}
+                    setUpdatingComment={setUpdatingComment}
+                    setCommentToUpdate={setCommentToUpdate}
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+        {updatingComment === false && (
+          <PostComment
+            article={article}
+            comentarios={comentarios}
+            setComentarios={setComentarios}
+            postCommentFunction={PostCommentArticle}
+          />
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default ArticleDetail;
--- src/Containers/Foro/PostDetail/PostDetail.jsx
+++ src/Containers/Foro/PostDetail/PostDetail.jsx
@@ -6,154 +6,224 @@ import FetchCommentsOfPost from '../../../Services/Forum/FetchCommentsOfPost';
 import CommentOfPost from '../../Blog/CommentArticle/CommentArticle';
 import UpdateComment from '../CommentOfPost/UpdateComment';
 import PutCommentOnPost from './PutCommentOnPost';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Avatar from "../../Common/Avatar";
 import FetchUserDetail from "../../../Services/User/FetchUserDetail";
 import { getDateFormated, getSignature } from "../../../Services/Common/Misc";
+import { TwitterShareButton, FacebookShareButton, WhatsappShareButton } from 'react-share';
+import { TwitterIcon, FacebookIcon, WhatsappIcon } from 'react-share';
+import { GoBroadcast } from 'react-icons/go';
 
 const PostDetail = () => {
 
-    const { postId } = useParams();
-    const [isLoading, setIsLoading] = useState();
-    const [post, setPost] = useState({});
-    const [topic, setTopic] = useState({});
-    const [comentarios, setComentarios] = useState([]);
-    const [updatingComment, setUpdatingComment] = useState(false);
-    const [commentToUpdate, setCommentToUpdate] = useState({});
-    const [autor, setAutor] = useState({});
+  const { pathname, hash } = useLocation();
 
-    useEffect(() => {
-        if (postId) {
-            setIsLoading(true);
-            let isSubscribed = true;
-            FetchPostDetail(postId).then(resp => {
-                if (isSubscribed) {
-                    setPost(resp);
-                }
-            });
-            FetchCommentsOfPost(postId).then(resp => {
-                if (isSubscribed) {
-                    setComentarios(resp.results);
-                    setIsLoading(false);
-                }
-            });
-            
-            return () => isSubscribed = false;
+  const { postId } = useParams();
+  const [isLoading, setIsLoading] = useState();
+  const [post, setPost] = useState({});
+  const [topic, setTopic] = useState({});
+  const [comentarios, setComentarios] = useState([]);
+  const [updatingComment, setUpdatingComment] = useState(false);
+  const [commentToUpdate, setCommentToUpdate] = useState({});
+  const [autor, setAutor] = useState({});
+
+  const hashtags = ["ComunidadLSA", "AprenderEsUnaActitud"];
+
+  useEffect(() => {
+    if (postId) {
+      setIsLoading(true);
+      let isSubscribed = true;
+      FetchPostDetail(postId).then(resp => {
+        if (isSubscribed) {
+          setPost(resp);
         }
-    }, [postId]);
-
-    useEffect(() => {
-        let isSubscribed = true;
-        if (post && post.autor) {
-            FetchUserDetail(post.autor).then(resp => {
-                if (isSubscribed) {
-                    setAutor(resp)
-                }
-            });
-            return () => isSubscribed = false;
+      });
+      FetchCommentsOfPost(postId).then(resp => {
+        if (isSubscribed) {
+          setComentarios(resp.results);
+          setIsLoading(false);
         }
-    }, [post]);
+      });
 
-        
+      return () => isSubscribed = false;
+    }
+  }, [postId]);
 
-    useEffect(() => {
-        if (post && post.tema) {
-            let isSubscribed = true;
-            FetchTopicDetail(post.tema).then(resp => {
-                if (isSubscribed) {
-                    setTopic(resp);
-                }
-            });
-            return () => isSubscribed = false;
+  useEffect(() => {
+    let isSubscribed = true;
+    if (post && post.autor) {
+      FetchUserDetail(post.autor).then(resp => {
+        if (isSubscribed) {
+          setAutor(resp)
         }
-    }, [post]);
+      });
+      return () => isSubscribed = false;
+    }
+  }, [post]);
 
-    const BreadCrumb = () => {
-        if (topic && topic.nombre) {
-            return (
-                <>
-                    <Link to={{ pathname: "/foro" }} className="text-xl border-b-2 border-transparent hover:border-blue-congreso200">Temas</Link>
-                    <span> / </span>
-                    <Link to={{ pathname: `/temas/${topic.id}/publicaciones` }} className="text-2xl border-b-2 border-transparent hover:border-blue-congreso200">{topic.nombre}</Link>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <Link to={{ pathname: "/foro" }} className="text-xl border-b-2 border-transparent hover:border-blue-congreso200">Temas</Link>
-                    <span> / </span>
-                </>
-            );
+
+
+  useEffect(() => {
+    if (post && post.tema) {
+      let isSubscribed = true;
+      FetchTopicDetail(post.tema).then(resp => {
+        if (isSubscribed) {
+          setTopic(resp);
         }
+      });
+      return () => isSubscribed = false;
+    }
+  }, [post]);
 
+  const BreadCrumb = () => {
+    if (topic && topic.nombre) {
+      return (
+        <>
+          <Link to={{ pathname: "/foro" }} className="text-xl border-b-2 border-transparent hover:border-blue-congreso200">Temas</Link>
+          <span> / </span>
+          <Link to={{ pathname: `/temas/${topic.id}/publicaciones` }} className="text-2xl border-b-2 border-transparent hover:border-blue-congreso200">{topic.nombre}</Link>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Link to={{ pathname: "/foro" }} className="text-xl border-b-2 border-transparent hover:border-blue-congreso200">Temas</Link>
+          <span> / </span>
+        </>
+      );
     }
 
-    return (
-      <div className="w-full overflow-hidden sm:w-3/4">
-        <div className="bg-yellow-congreso98 font-Midnight text-red-congreso200 pt-3 pb-3 pl-5">
-          <BreadCrumb />
-        </div>
-        <div className="p-8">
-          <div className="flex justify-between items-center h-16 my-6 ">
-            <div className="flex items-center">
-              <Avatar userId={post.autor} />
-              <div className="ml-2">
-                <div className="text-2xl font-bold">{post.titulo}</div>
-                <div className="text-sm font-light text-gray-500">
-                  {getSignature(autor)} {getDateFormated(post.fecha_creacion)}
-                </div>
+  }
+
+  return (
+    <div className="w-full overflow-hidden sm:w-3/4">
+      <div className="bg-yellow-congreso98 font-Midnight text-red-congreso200 pt-3 pb-3 pl-5">
+        <BreadCrumb />
+      </div>
+      <div className="p-3">
+        <div className="flex justify-between items-center h-16 my-6 ">
+          <div className="flex items-center">
+            <Avatar userId={post.autor} />
+            <div className="ml-2">
+              <div className="text-2xl font-bold">{post.titulo}</div>
+              <div className="text-sm font-light text-gray-500">
+                {getSignature(autor)} {getDateFormated(post.fecha_creacion)}
               </div>
             </div>
           </div>
-          {/* <p className="text-2xl font-bold pl-4">{post.titulo}</p> */}
-          <div className="w-full p-5">
-            <article
-              className=""
-              dangerouslySetInnerHTML={{ __html: post.cuerpo }}
-            ></article>
+        </div>
+        {/* <p className="text-2xl font-bold pl-4">{post.titulo}</p> */}
+        <div className="w-full p-5">
+          <article
+            className=""
+            dangerouslySetInnerHTML={{ __html: post.cuerpo }}
+          ></article>
+        </div>
+        <div className="hidden sm:flex w-full sm:flex-shrink-0 sm:justify-center mb-5 text-3xl font-bold">
+          <h3><GoBroadcast className="inline" /> ¡Dale difusión!</h3>
+        </div>
+
+        <div className="hidden sm:flex w-full sm:flex-shrink-0 sm:justify-center">
+          <TwitterShareButton
+            url={process.env.REACT_APP_FRONTEND_URL + pathname}
+            title={post.titulo}
+            hashtags={hashtags}
+            related={["AprendizajeExt"]}
+            className="mx-3"
+          >
+            <TwitterIcon size={42} round={true} />
+          </TwitterShareButton>
+          <FacebookShareButton
+            url={process.env.REACT_APP_FRONTEND_URL + pathname}
+            quote={post.titulo + " " + post.entradilla}
+            hashtags={hashtags}
+            className="mx-3"
+          >
+            <FacebookIcon size={42} round={true} />
+          </FacebookShareButton>
+          <WhatsappShareButton
+            url={process.env.REACT_APP_FRONTEND_URL + pathname}
+            title={post.titulo}
+            className="mx-3"
+          >
+            <WhatsappIcon size={42} round={true} />
+          </WhatsappShareButton>
+        </div>
+
+        <div className="sm:hidden flex w-full justify-center">
+          <div className="mx-1">
+            <TwitterShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              title={post.titulo}
+              hashtags={hashtags}
+              related={["AprendizajeExt"]}
+            >
+              <TwitterIcon size={42} round={true} />
+            </TwitterShareButton>
           </div>
-          <div id="article-detail-comments" className="">
-            <p className="text-xl font-bold p-3">Comentarios</p>
-            <div>
-              {comentarios.map((comment, i) => {
-                if (
-                  updatingComment &&
-                  commentToUpdate &&
-                  commentToUpdate.id === comment.id
-                ) {
-                  return (
-                    <UpdateComment
-                      key={i}
-                      comment={comment}
-                      comentarios={comentarios}
-                      setComentarios={setComentarios}
-                      setUpdatingComment={setUpdatingComment}
-                    />
-                  );
-                } else {
-                  return (
-                    <CommentOfPost
-                      key={i}
-                      comment={comment}
-                      commentIdx={i}
-                      setUpdatingComment={setUpdatingComment}
-                      setCommentToUpdate={setCommentToUpdate}
-                    />
-                  );
-                }
-              })}
-            </div>
+          <div className="mx-1">
+            <FacebookShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              quote={post.titulo + " " + post.entradilla}
+              hashtags={hashtags}
+            >
+              <FacebookIcon size={42} round={true} />
+            </FacebookShareButton>
           </div>
-          {updatingComment === false && (
+          <div className="mx-1">
+            <WhatsappShareButton
+              url={process.env.REACT_APP_FRONTEND_URL + pathname}
+              title={post.titulo}
+            >
+              <WhatsappIcon size={42} round={true} />
+            </WhatsappShareButton>
+          </div>
+        </div>
+
+        <div id="article-detail-comments" className="">
+          <p className="text-xl font-bold p-3">Comentarios</p>
+          <div>
+            {comentarios.map((comment, i) => {
+              if (
+                updatingComment &&
+                commentToUpdate &&
+                commentToUpdate.id === comment.id
+              ) {
+                return (
+                  <UpdateComment
+                    key={i}
+                    comment={comment}
+                    comentarios={comentarios}
+                    setComentarios={setComentarios}
+                    setUpdatingComment={setUpdatingComment}
+                  />
+                );
+              } else {
+                return (
+                  <CommentOfPost
+                    key={i}
+                    comment={comment}
+                    commentIdx={i}
+                    setUpdatingComment={setUpdatingComment}
+                    setCommentToUpdate={setCommentToUpdate}
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+        {
+          updatingComment === false && (
             <PutCommentOnPost
               post={post}
               comentarios={comentarios}
               setComentarios={setComentarios}
             />
-          )}
-        </div>
-      </div>
-    );
+          )
+        }
+      </div >
+    </div >
+  );
 }
 
 export default PostDetail;
