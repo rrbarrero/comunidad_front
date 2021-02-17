e05eab6147ea4de1ba9a4eed33578b6a17d24671
Roberto Rodríguez <rbarrero@posteo.net>
2021-02-17 11:14:58 +0100

    Arreglos en el munú para móviles

--- src/Containers/Navbar/NavbarMobile.jsx
+++ src/Containers/Navbar/NavbarMobile.jsx
@@ -2,13 +2,15 @@ import { useState, useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../App';
+import { Spin as Hamburger } from 'hamburger-react'
 
 const NavbarMobile = () => {
 
-    const {isAuthenticated, currentUser} = useContext(UserContext);
+    const { isAuthenticated, currentUser } = useContext(UserContext);
     const [displayClass, setDisplayClass] = useState("none");
+    const [menuIsOpen, menuSetOpen] = useState(false);
 
-    const expandOptions = () =>  {
+    const expandOptions = () => {
         if (displayClass === "block") {
             setDisplayClass("none");
         } else {
@@ -17,27 +19,22 @@ const NavbarMobile = () => {
     }
 
     return (
-       <div className="lg:hidden container mx-auto bg-gray-200 p-3">
-        <nav className="flex-row lg:justify-between">
-            <div className="flex flex-row justify-between">
+        <div className="lg:hidden container mx-auto bg-gray-200 p-3">
+            <nav className="flex-row lg:justify-between">
+                <div className="flex flex-row justify-between">
+                    <Link to={{ pathname: "/" }} className="py-1 mt-2 px-0 font-Midnight text-xl text-red-congreso200 block text-md">Inicio </Link>
+                    <Link to={{ pathname: "/foro" }} className="py-1 mt-2 px-0 font-Midnight text-2xl text-red-congreso200 block text-md">Foro </Link>
+                    <Hamburger toggled={menuIsOpen} toggle={menuSetOpen} size={36} color="#df674f" rounded />
+                </div>
 
-                <Link to={{ pathname: "/" }} className="py-1 px-0 font-Midnight text-xl text-red-congreso200 block text-md">Inicio </Link>
-                <Link to={{ pathname: "/foro" }} className="py-1 px-0 font-Midnight text-xl text-red-congreso200 block text-md">Foro </Link>
-
-            <button id="hamburgerbtn" className="lg:hidden text-3xl bg-gray-200 text-red-congreso200" onClick={expandOptions}>
-                <FaBars />
-            </button>
-            </div>
-        
-            <ul className="lg:hidden" id="mobileMenu" style={{display: displayClass}}>
-                <li className="pr-5"><Link to={{ pathname: "/foro" }} className="text-red-congreso200 pl-4 font-Midnight">Foro </Link></li>
-                {!isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/login" }} className="text-red-congreso200 pl-4 font-Midnight">Iniciar sesión </Link></li>}
-                {!isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/registro" }} className="text-red-congreso200 pl-4 font-Midnight">Registro </Link></li>}
-                {isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: `/perfil/${currentUser.userId}` }} className="text-red-congreso200 pl-4 font-Midnight">Mi perfil </Link></li>}
-                {isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/logout" }} className="text-red-congreso200 pl-4 font-Midnight">Cerrar sesión </Link></li>}
-            </ul>
-        </nav>
-    </div>
+                {menuIsOpen && <ul id="mobileMenu">
+                    {!isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/login" }} className="text-red-congreso200 pl-4 text-2xl font-Midnight">Iniciar sesión </Link></li>}
+                    {!isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/registro" }} className="text-red-congreso200 pl-4 text-2xl font-Midnight">Registro </Link></li>}
+                    {isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: `/perfil/${currentUser.userId}` }} className="text-red-congreso200 pl-4 text-xl font-Midnight">Mi perfil </Link></li>}
+                    {isAuthenticated && <li className="pr-5 my-3"><Link to={{ pathname: "/logout" }} className="text-red-congreso200 pl-4 font-Midnight">Cerrar sesión </Link></li>}
+                </ul>}
+            </nav>
+        </div>
     );
 }
 
